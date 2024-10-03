4eff41a5a0d6b09fb5567d5b3a547547a84a60ac
WilliansonDantas <williansondantas@gmail.com>
2024-10-03 16:43:56 -0300

    refactor: updated the AppModule to establish a TypeORM connection to a MySQL database for managing Capivara entities. Integrated the CapivaraModule to enhance modularity and maintainability of the application

--- capivara-management/src/app.module.ts
+++ capivara-management/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Capivara } from './capivara/capivara.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CapivaraModule } from './capivara/capivara.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'capivara_management',
+      entities: [Capivara],
+      synchronize: true,
+    }),
+    CapivaraModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
